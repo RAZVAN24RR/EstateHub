d6a17fbe0713bb659ea794ac5cabfad56b2532e6
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-04-04 01:50:36 +0300

    Configurare

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//citeste ce inseamna routing pe o aplicatie ts/js/React.
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Aici trebuie sa scrii cod, Adelin :)).</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/api/networking.ts
@@ -0,0 +1,3 @@
+export {};
+// Trebuie implementat aici o instanta de Axios
+//Vezi ce e axios

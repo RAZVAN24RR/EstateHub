5595f886a2cd2a1255efbe93e2fbde3628473a66
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-05-12 15:28:44 +0300

    allow * pe cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,12 @@ const app = express();
 const port = 5000;
 const baseApp = `/api/v1`;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+  })
+);
 app.use(bodyParser.json());
 
 app.use(`${baseApp}`, router);

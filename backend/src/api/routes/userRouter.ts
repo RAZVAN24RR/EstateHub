2577874da40919e3193167bc1a8fdde15b6da414
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-05-20 14:25:02 +0300

    data format

--- backend/src/api/routes/userRouter.ts
+++ backend/src/api/routes/userRouter.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from "express";
 import { Router } from "express";
 import userService from "../services/userService";
+import { CreatedAt, UpdatedAt } from "sequelize-typescript";
+import dataFormat from "../../utils/dataFormat";
 
 const userRouter = Router();
 
 userRouter.get("/:id", async (req: Request, res: Response) => {
   const id = Number(req.params.id);
   const result = await userService.getById(id);
-  return res.status(200).send(result);
+  return res.status(200).send({
+    name: result.name,
+    email: result.email,
+    createdAt: dataFormat(result.createdAt),
+    updatedAt: dataFormat(result.updatedAt),
+  });
 });
 userRouter.delete("/:id", async (req: Request, res: Response) => {
   const id = Number(req.params.id);
